14a7bc9beee7d01dccd2825a5af7c0b9b8ab2e70
Tech-Addict <davidckihoto@gmail.com>
2023-02-20 14:04:35 -0800

    Prints a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - entry point
+ * Description: "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
 {
 	char str[] = "Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
