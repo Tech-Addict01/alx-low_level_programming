7ef7595e05f740d28fc0ba0a2746ec81d6d06190
Tech-Addict <davidckihoto@gmail.com>
2023-02-20 18:50:50 -0800

    Print 10 number from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descritpion: print numbers of base ten from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+
+	return (0);
+}
