4e6d0a0fc60546379cbda716cbf9f6606189ad61
Tech-Addict <davidckihoto@gmail.com>
2023-02-24 14:58:19 -0800

    do not print 2 and 4 from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ *
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a != '2' && a != '4')
+	{
+		for (a = '0'; a <= '9'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
