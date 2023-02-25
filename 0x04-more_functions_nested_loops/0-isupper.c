d033a30eec0b4aea373276c908af218e0cb30056
Tech-Addict <davidckihoto@gmail.com>
2023-02-25 12:02:48 -0800

    Check if letter is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function name
+ * Description: checks if letter is uppercase
+ * Return: 1 (success) 0 (fail)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
