e25504a7cdd1c83a20bc1f78d6d2c9016a763a82
Tech-Addict <davidckihoto@gmail.com>
2023-02-28 08:40:08 -0800

    Using pointer to reset value of another pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes value of pointer to 98
+ * @*n: pointer to an int
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int a = 89;
+	int *p = &a;
+	*n = *p;
+
+	*n = 98;
+}
