33b9cb67ba6721a72836e7f50a360c1b180edd91
Tech-Addict <davidckihoto@gmail.com>
2023-03-03 15:15:34 -0800

    Print numbers to 14 with _putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 - 14.
+ *
+ * Return: void.
+ */
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if ( m > 9)
+			{
+				_putchar(m / 10 + '0');
+			}
+			_putchar(m % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
